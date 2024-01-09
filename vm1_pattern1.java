b64710df8558d604ce86685567b3d08ed7b84cb7
vanshmaniya27 <vanshmaniya8250@gmail.com>
2024-01-10 00:20:11 +0530

    add and delete

---
+++ vm1_pattern1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class vm1_pattern1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //String name = sc.nextline();
+        // next
+        String name = sc.next();
+
+        System.out.println(name);
+    }
+}
--- vm8.java
+++
@@ -1,5 +0,0 @@
-public class vm8 {
-    public static void main(String[] args) {
-        
-    }
-}
